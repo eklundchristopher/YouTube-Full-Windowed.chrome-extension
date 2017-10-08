f6069a4cffc542e5f3737de9765160a6202d13bb
Christopher Eklund <eklundchristopher@gmail.com>
2017-10-08 21:00:40 +0200

    Rewritten from scratch

--- assets/embed.js
+++ assets/embed.js
@@ -1,19 +1,103 @@
-(function (chrome, ytbfw) {
+(function (chrome) {
+    let button, theatre
+    let state = false, theatreState = false, initialised = false
 
-    var attempts = 0
-    var available = setInterval(function () {
-        attempts++
+    const options = {
+        namespace: 'YouTube Full Windowed Extension',
+        icons: {
+            open: `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytbfw-id-1"></use><path stroke="#000" fill="#fff" stroke-width="0" stroke-opacity="null" d="m13.56101,18.08987l-0.00013,4.34952l4.34952,-0.00013l0.00066,2.48645l-6.83597,-0.00054l-0.00079,-6.83624" id="ytbfw-id-1"/><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytbfw-id-2"></use><path transform="rotate(-180 21.507320404052734,14.492679595947264) " stroke="#000" fill="#fff" stroke-width="0" stroke-opacity="null" d="m20.57565,11.07523l-0.00013,4.34952l4.34952,-0.00013l0.00066,2.48645l-6.83597,-0.00054l-0.00079,-6.83624" id="ytbfw-id-2"/></svg>`,
+            close: `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytbfw-id-5"></use><path transform="rotate(-180 12.460127830505371,23.539867401123047) " id="ytbfw-id-5" d="m11.52846,20.12242l-0.00013,4.34952l4.34952,-0.00013l0.00066,2.48645l-6.83597,-0.00054l-0.00079,-6.83624" stroke-opacity="null" stroke-width="0" fill="#fff" stroke="#000"/><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytbfw-id-6"></use><path id="ytbfw-id-6" d="m22.6082,9.04268l-0.00013,4.34952l4.34952,-0.00013l0.00066,2.48645l-6.83597,-0.00054l-0.00079,-6.83624" stroke-opacity="null" stroke-width="0" fill="#fff" stroke="#000"/></svg>`,
+            hover: {
+                open: `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytbfw-id-3"></use><path stroke="#000" fill="#fff" stroke-width="0" stroke-opacity="null" d="m12.10554,19.54534l-0.00013,4.34952l4.34952,-0.00013l0.00066,2.48645l-6.83597,-0.00054l-0.00079,-6.83624" id="ytbfw-id-3"/><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytbfw-id-4"></use><path transform="rotate(-180 22.962797164916992,13.037205696105957) " stroke="#000" fill="#fff" stroke-width="0" stroke-opacity="null" d="m22.03112,9.61976l-0.00013,4.34952l4.34952,-0.00013l0.00066,2.48645l-6.83597,-0.00054l-0.00079,-6.83624" id="ytbfw-id-4"/></svg>`,
+                close: `<svg height="100%" version="1.1" viewBox="0 0 36 36" width="100%"><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytbfw-id-7"></use><path transform="rotate(-180 14.062260627746582,21.9377384185791) " id="ytbfw-id-7" d="m13.13059,18.52029l-0.00013,4.34952l4.34952,-0.00013l0.00066,2.48645l-6.83597,-0.00054l-0.00079,-6.83624" stroke-opacity="null" stroke-width="0" fill="#fff" stroke="#000"/><use class="ytp-svg-shadow" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#ytbfw-id-8"></use><path id="ytbfw-id-8" d="m21.00607,10.64481l-0.00013,4.34952l4.34952,-0.00013l0.00066,2.48645l-6.83597,-0.00054l-0.00079,-6.83624" stroke-opacity="null" stroke-width="0" fill="#fff" stroke="#000"/></svg>`,
+            }
+        },
+        events: {
+            open: function () {
+                document.body.classList.add('extension-full-windowed-enabled')
 
-        var toolbar = document.querySelector('.ytp-chrome-controls .ytp-right-controls')
-        if (toolbar !== null) {
-            chrome.runtime.sendMessage({ action: 'initialise' }, function () {
-                ytbfw.init(toolbar)
+                if (! theatreState) {
+                    theatre.click()
+                    theatreState = ! theatreState
+                }
+
+                window.dispatchEvent(new Event('resize'))
+            },
+            close: function () {
+                document.body.classList.remove('extension-full-windowed-enabled')
+
+                if (theatreState) {
+                    theatre.click()
+                    theatreState = ! theatreState
+                }
+
+                window.dispatchEvent(new Event('resize'))
+            }
+        }
+    }
+
+    document.querySelector('video.html5-main-video').addEventListener('playing', function (event) {
+        if (initialised === true) {
+            return
+        }
+
+        document.body.classList.add('extension-full-windowed')
+
+        theatre = document.querySelector('.ytp-size-button')
+        theatreState = theatre.getAttribute('title') === 'Default view'
+
+        chrome.runtime.onMessage.addListener(function (request, sender, callback) {
+            switch (request.action) {
+                case 'toggle':
+                    state = request.state
+
+                    if (button !== undefined) {
+                        button.innerHTML = options.icons[state ? 'close' : 'open']
+                    }
+
+                    options.events[state ? 'open' : 'close']()
+
+                    console.log(`[${options.namespace}]: ${state ? 'Opened' : 'Closed'}`)
+                    break
+            }
+        })
+
+        chrome.runtime.sendMessage({ action: 'initialise', state }, function () {
+            button = document.createElement('button')
+
+            button.setAttribute('class', 'ytp-full-windowed-button ytp-button')
+            button.setAttribute('title', 'Full Windowed')
+
+            button.innerHTML = options.icons.open
+
+            button.addEventListener('mouseover', function () {
+                this.innerHTML = options.icons.hover[state ? 'close' : 'open']
             })
-        }
 
-        if (attempts >= 15 || toolbar !== null) {
-            clearInterval(available)
-        }
-    }, 500)
+            button.addEventListener('mouseout', function () {
+                this.innerHTML = options.icons[state ? 'close' : 'open']
+            })
 
-})(chrome, ytbfw)
+            button.addEventListener('click', function (event) {
+                chrome.runtime.sendMessage({ action: 'toggle' }, function (state) {
+                    options.events[state ? 'open' : 'close']()
+                })
+            })
+
+            document.body.addEventListener('keyup', function (e) {
+                if (state === true && e.keyCode == 27) {
+                    chrome.runtime.sendMessage({ action: 'toggle' }, function (state) {
+                        options.events.close()
+                    })
+                }
+            })
+
+            let toolbar = document.querySelector('.ytp-chrome-controls .ytp-right-controls')
+            toolbar.childNodes[1].insertAdjacentElement('afterend', button)
+
+            console.log(`[${options.namespace}]: Initialised`)
+        })
+
+        initialised = true
+    }, false)
+})(chrome)
--- assets/extension.js
+++ assets/extension.js
@@ -1,52 +1,124 @@
 (function (chrome) {
-    var initialised = []
+    let namespace = 'YouTube Full Windowed Extension'
+    let instance = new Extension()
+    
+    /**
+     * Listen for and execute certain extension actions.
+     *
+     * @param  object  request
+     * @param  object  sender
+     * @param  closure  callback
+     * @return void
+     */
+    chrome.runtime.onMessage.addListener(function (request, sender, callback) {
+        var action = request.action || ''
 
+        instance.log(`${action}()`, false)
+
+        if (instance[action] === undefined) {
+            throw new Error(`Unknown Action -> ${action}`)
+        }
+
+        instance[action](request, sender, callback)
+    })
+
+    /**
+     * Listen for when the current tab has finished loading.
+     *
+     * @param  integer  tabId
+     * @param  object  changeInfo
+     * @param  object  tab
+     * @return void
+     */
     chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-        var pattern = /^https:\/\/www.youtube.com\/watch\?v=/i
+        if (changeInfo.status !== 'complete') {
+            return
+        }
 
-        if (changeInfo.status === 'complete') {
-            if (pattern.test(tab.url)) {
-                chrome.browserAction.setIcon({ path: 'icons/enter-fw.png', tabId: tabId })
-                chrome.browserAction.setTitle({ title: 'Enter Full Windowed', tabId: tabId })
-            } else {
-                chrome.browserAction.setIcon({ path: 'icons/browser.png', tabId: tabId })
-                chrome.browserAction.setTitle({ title: 'YouTube Full Windowed', tabId: tabId })
+        chrome.browserAction.setIcon({ path: 'icons/state-0.png', tabId })
+        chrome.browserAction.setTitle({ title: 'YouTube Full Windowed', tabId })
+    })
 
-                delete initialised[tabId]
+    /**
+     * Trigger a toggle event when the extension browser button is pressed.
+     *
+     * @param  object  tab
+     * @return void
+     */
+    chrome.browserAction.onClicked.addListener(function (tab) {
+        if (instance.state[tab.id] === undefined) {
+            return
+        }
+
+        instance.log(`toggle()`, false)
+        instance.toggle({}, { tab })
+    })
+
+
+
+    function Extension () {
+        this.state = {}
+
+        /**
+         * Log information to the extension's background page.
+         *
+         * @param  string  message
+         * @param  boolean  response  true
+         * @return void
+         */
+        this.log = function (message, response = true) {
+            var indentation = response === true ? '   ->   ' : ''
+
+            chrome.extension.getBackgroundPage().console.log(
+                `[${namespace}]: ${indentation}${message}`
+            )
+        }
+
+        /**
+         * Initialise the extension.
+         *
+         * @param  object  request
+         * @param  object  sender
+         * @param  closure  callback
+         * @return void
+         */
+        this.initialise = function (request, sender, callback) {
+            this.state[sender.tab.id] = request.state
+
+            this.log(`Extension was initialised with state ${this.state[sender.tab.id]}`)
+
+            chrome.browserAction.setIcon({ path: 'icons/state-1.png', tabId: sender.tab.id })
+            chrome.browserAction.setTitle({ title: 'Enter Full Windowed', tabId: sender.tab.id })
+
+            if (callback) {
+                callback()
             }
         }
-    })
 
-    chrome.runtime.onMessage.addListener(function (request, sender, callback) {
-        var action = (request.action || '').toLowerCase()
+        /**
+         * Toggle the state of the extension.
+         *
+         * @param  object  request
+         * @param  object  sender
+         * @param  closure  callback
+         * @return void
+         */
+        this.toggle = function (request, sender, callback) {
+            let state = this.state[sender.tab.id] = ! this.state[sender.tab.id]
 
-        switch (action) {
-            case 'initialise':
-                delete initialised[sender.tab.id]
-                initialised.push(sender.tab.id)
-                callback()
-                break;
+            let path = state === false ? 'icons/state-1.png' : 'icons/state-2.png'
+            let title = state === false ? 'Enter Full Windowed' : 'Exit Full Windowed'
 
-            case 'toggle':
-                var path = request.status === true ? 'icons/exit-fw.png' : 'icons/enter-fw.png'
-                var title = request.status === true ? 'Exit Full Windowed' : 'Enter Full Windowed'
+            chrome.browserAction.setIcon({ path, tabId: sender.tab.id })
+            chrome.browserAction.setTitle({ title, tabId: sender.tab.id })
 
-                chrome.browserAction.setIcon({ path: path, tabId: sender.tab.id })
-                chrome.browserAction.setTitle({ title: title, tabId: sender.tab.id })
-                break;
+            this.log(`State was updated to ${state}`)
+
+            chrome.tabs.sendMessage(sender.tab.id, { action: 'toggle', state })
+
+            if (callback) {
+                callback(state)
+            }
         }
-    })
-
-    chrome.browserAction.onClicked.addListener(function (tab) {
-        var pattern = /^https:\/\/www.youtube.com\/watch\?v=/i
-        if (initialised.indexOf(tab.id) === -1 || ! pattern.test(tab.url)) {
-            return false;
-        }
-
-        chrome.tabs.executeScript(tab.id, {
-            code: 'ytbfw.toggle()',
-        }, function (response) {
-            chrome.runtime.sendMessage({ action: 'toggle', status: response[0] })
-        })
-    })
+    }
 })(chrome)
